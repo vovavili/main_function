915f44c69191898c8bb4b0398f7e8f9baeb73fba
Vova <vladimirvilimaitis@gmail.com>
2022-08-31 20:40:16 +0200

    Added an ability to defer execution until the script has been fully initialized.

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vovavili/main_function',
-    version='1.0.4',
+    version='1.0.5',
     zip_safe=False,
 )
